c76287e3e937abc4a7e6bf675718466b2c01bb54
Akshat <iitkakshat@gmail.com>
2024-09-29 14:18:16 +0530

    Add initial project files and dependencies

---
+++ models/Models.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from ..db import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role_id = Column(Integer, ForeignKey('roles.id'))
+
+class Role(Base):
+    __tablename__ = "roles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    description = Column(String)
+
+class Otp(Base):
+    __tablename__ = "otps"
+
+    id = Column(Integer, primary_key=True, index=True)
+    otp = Column(String, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    is_active = Column(Boolean, default=True)
+
+
