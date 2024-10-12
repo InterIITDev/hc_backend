5eb81bbbc1c973892f31056bdbec96685719cb7a
IceShuttle <IceShuttle@linux>
2024-10-13 03:50:13 +0530

    user register and login done

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from db import setup_db
+from user.auth import router
+
+setup_db()
+app = FastAPI()
+app.include_router(router,prefix="/auth")
+
+
+@app.post("/")
+def create_user():
+    return {"Hello": "World"}
+
+
