994ca36798d2afe510212909d96a32b35c3c9ce7
IceShuttle <IceShuttle@Debian>
2024-10-15 15:12:17 +0530

    implemented appointment api

--- appointments.py
+++ appointments.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,status
 from sqlalchemy.orm import Session
+from sqlalchemy import select
 from db import get_engine
 from models import Appointments
 from pydantic import BaseModel
@@ -17,6 +18,33 @@ def book_appointment(info:NewAppointment):
         appointment = Appointments(patient_id=info.username,desc=info.desc,last_updated_by=info.username)
         session.add(appointment)
         session.commit()
+
+@router.get("/{id}")
+def get_appointment(id:int):
+    stmt = select(Appointments).where(Appointments.id==id)
+    engine = get_engine()
+    with Session(engine) as session:
+        user = session.scalars(stmt).one_or_none()
+        return user
+
+
+class UpdateAppointment(BaseModel):
+    doctor_id: str | None
+    desc: str | None
+
+@router.put("/{id}")
+def update_appointment(id:int,updates:UpdateAppointment):
+    stmt = select(Appointments).where(Appointments.id==id)
+    engine = get_engine()
+    with Session(engine) as session:
+        user = session.scalars(stmt).one_or_none()
+        if user is not None:
+            if updates.doctor_id is not None:
+                setattr(user,'doctor_id',updates.doctor_id)
+            if updates.desc is not None:
+                setattr(user,'desc',updates.desc)
+        session.commit()
+        return status.HTTP_200_OK
 
 
 
